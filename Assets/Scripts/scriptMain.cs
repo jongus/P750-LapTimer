9945f1732b8bf4a185ba58b55ee3b925941efab9
Jonas Gustafson <jonas@pixotag.com>
2013-01-10 22:15:55 +0100

    Added and tested GpsStatusOkey

--- Assets/Scripts/scriptMain.cs
+++ Assets/Scripts/scriptMain.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class scriptMain : MonoBehaviour {
 	private string sStatus = "-";
-	tk2dTextMesh tmStatus;
-	tk2dTextMesh tmLatLon;
+	private LocationInfo liOld;
+	private LocationInfo liNew;
+	private tk2dTextMesh tmStatus;
+	private tk2dTextMesh tmLatLon;
+	
 	 
 	// Start is called just before any of the
 	// Update methods is called the first time.
@@ -19,22 +23,54 @@ public class scriptMain : MonoBehaviour {
 	// Update is called every frame, if the
 	// MonoBehaviour is enabled.
 	void Update () {
+		//Check for gps operation
+		
 		//tmLatLon.text = Input.location.lastData.latitude.ToString ();
-		if(Input.location.status == LocationServiceStatus.Running){
-			tmLatLon.text = Input.location.lastData.latitude.ToString () + "\n" + 
-				Input.location.lastData.longitude.ToString () + "\n" +
-					Input.location.lastData.horizontalAccuracy.ToString() + "\n" +
-					Input.location.lastData.timestamp.ToString();	
+		if(GpsStatusOkey() == 2){
+			tmLatLon.text = liNew.latitude.ToString () + "\n" + 
+				liNew.longitude.ToString () + "\n" +
+					liNew.horizontalAccuracy.ToString() + "\n" +
+					liNew.timestamp.ToString()  + "\n" +
+					NowInEpoch ();
+		} else if(GpsStatusOkey() == 1) {
+			tmLatLon.text = "ORANGE" + "\nAccuracy:" + liNew.horizontalAccuracy.ToString() + "\nDiff:" + 
+				(NowInEpoch() - liNew.timestamp).ToString();
+		} else {
+			tmLatLon.text = "RED";
 		}
 		tmLatLon.Commit ();
 		
-		sStatus = Input.location.status.ToString ();
+		sStatus = Input.location.status.ToString () + " : " + NowInEpoch ();
 		tmStatus.text = sStatus;
 		tmStatus.Commit ();
 	}
 	
-	void OnBecameInvisible() {
-        Input.location.Stop ();
-    }
+	int GpsStatusOkey() {
+		//0 = false 1 = orange, some kind but not good, 2 = working good!'
+		if(Input.location.status == LocationServiceStatus.Running ) {
+			liNew = Input.location.lastData;
+			//Check last update time
+			if((NowInEpoch() - liNew.timestamp) > 5.0d ) {
+				//okey, more than 5 sec = Orange
+				return 1;
+			}
+			
+			//Check accuracy
+			if(liNew.horizontalAccuracy > 30.0f){
+				//Too bad = Orange
+				return 1;
+			}
+			//Okey, every thing is okey, I think
+			return 2;
+			
+		} else {
+			return 0;
+		}
+	}
+	
+	double NowInEpoch() {
+		TimeSpan span = DateTime.UtcNow.Subtract (new DateTime(1970,1,1,0,0,0));
+		return (double)span.TotalSeconds;
+	}
 	
 }
